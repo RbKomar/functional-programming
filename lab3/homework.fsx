3915493e95289f12d9c45732adaa344f5c43773a
Robert Komar <robkomar98@gmail.com>
2021-11-18 20:18:01 +0100

    ex 3.1 - solved

---
+++ lab3/homework.fsx
@@ -0,0 +1,171 @@
+let SHOW x = printf "%A\n" x
+//### Exercise 3.1
+//##Implement `parseScore`.
+
+//#### --------------- Your code goes below --------------- *)
+let rec parseScore (chars: char list): int option list = []
+
+let ``exercise 3.1`` =
+    parseScore [ 'X'
+                 '4'
+                 '/'
+                 '2'
+                 '-'
+                 'N' ]
+//** #### Value of ``exercise 3.1`` *)
+SHOW ``exercise 3.1``
+
+//### Exercise 3.2
+//##Implement `countScore`
+
+//#### --------------- Your code goes below --------------- *)
+let rec countScore (scores: int list): int = 0
+
+let ``exercise 3.2`` =
+    [ [ 10
+        10
+        10
+        10
+        10
+        10
+        10
+        10
+        10
+        10
+        10
+        10 ]
+      [ 9
+        0
+        9
+        0
+        9
+        0
+        9
+        0
+        9
+        0
+        9
+        0
+        9
+        0
+        9
+        0
+        9
+        0
+        9
+        0 ]
+      [ 5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5
+        5 ]
+      [ 10
+        9
+        1
+        5
+        5
+        7
+        2
+        10
+        10
+        10
+        9
+        0
+        8
+        2
+        9
+        1
+        10 ] ]
+    |> List.map countScore
+//** #### Value of ``exercise 3.2`` *)
+SHOW ``exercise 3.2``
+
+//### sequenceOpts function *)
+let sequenceOpts (optionals: 'a option list): 'a list option =
+    let rec sequence' acc optionals =
+        match optionals, acc with
+        | [], _ -> Option.map List.rev acc
+        | Some h :: t, Some acc -> sequence' (Some(h :: acc)) t
+        | _ -> None
+
+    sequence' (Some []) optionals
+
+
+//### Homework 1
+//##Implement `bowlingScore`.
+
+//###Hint: Use `sequenceOpts` to convert from list of options to option of list
+let bowlingScore (score: string): int option = Some 0
+
+let ``homework 1`` =
+    [ "XXXXXXXXXXXX"
+      "9-9-9-9-9-9-9-9-9-9-"
+      "9--/9-9-9-9-9-9-9-9-"
+      "X-/9-9-9-9-9-9-9-9-"
+      "9-X9-9-X--9-9-9-9-"
+      "9-9-9-9-9-9-9-9-9-9-"
+      "9-9-9-9-9-9-9-9-9-XXX"
+      "5/5/5/5/5/5/5/5/5/5/5"
+      "5/5/5/5/5/5/5/5/5/5/X"
+      "X9/5/72XXX9-8/9/X" ]
+    |> List.map bowlingScore
+
+//** #### Value of ``homework 1`` *)
+SHOW ``homework 1``
+
+//EXPECTED RESULTS   
+//[Some 300; Some 90; Some 100; Some 111; Some 92;
+//   Some 90; Some 111; Some 150; Some 155; Some 187]
+
+
+//### Homework 2
+//###Write new, **tail-recursive** versions of `parseScore` and `countScore`.
+//###Implement `bowlingScoreTail` to use those 2 new functions
+
+let rec parseScoreTail (chars: char list) (acc: int option list): int option list = []
+
+let rec countScoreTail (scores: int list) (acc: int): int = 0
+
+let bowlingScoreTail (score: string): int option = Some 0
+
+let ``homework 2`` =     
+    [ "XXXXXXXXXXXX"
+      "9-9-9-9-9-9-9-9-9-9-"
+      "9--/9-9-9-9-9-9-9-9-"
+      "X-/9-9-9-9-9-9-9-9-"
+      "9-X9-9-X--9-9-9-9-"
+      "9-9-9-9-9-9-9-9-9-9-"
+      "9-9-9-9-9-9-9-9-9-XXX"
+      "5/5/5/5/5/5/5/5/5/5/5"
+      "5/5/5/5/5/5/5/5/5/5/X"
+      "X9/5/72XXX9-8/9/X" ]
+    |> List.map bowlingScoreTail 
+//** #### Value of ``homework 2`` *)
+SHOW ``homework 2``
+
+//EXPECTED RESULTS   
+//[Some 300; Some 90; Some 100; Some 111; Some 92;
+//   Some 90; Some 111; Some 150; Some 155; Some 187]
+
+//////////////////////////////////////////////////////////////
+/// Indeks:
+/// Imię:
+/// Nazwisko:
+/// 
+/// Podsumowanie zalizowanych zadan:
